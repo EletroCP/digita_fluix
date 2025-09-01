7ddbcafe97c7f7733fa04975e4ed1e411512b2ca
Breno L. Lopes <brernolopes@hotmail.com>
2025-09-01 18:55:11 -0300

    feat: :sparkles: Adicionando rotas

---
+++ src/Rotas.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './paginas/Login';
+//import Senha from './pages/Senha';
+//import Home from './pages/Home';
+import App from './App';
+
+function Rotas() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/*<Route path="/senha" element={<Senha />} />*/}
+        <Route path="/home" element={<App />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default Rotas;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
+import Rotas from './Rotas';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Rotas />
   </React.StrictMode>
 );
 
