9de502c82dcdcd0f8198ecb388ec85fb02b44ba6
Breno L. Lopes <brernolopes@hotmail.com>
2025-09-01 19:18:07 -0300

    feat: :lipstick: Cor custom para tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'off-white': '#F5F5F5',
+      }
+    },
   },
   variants: {
     extend: {},
